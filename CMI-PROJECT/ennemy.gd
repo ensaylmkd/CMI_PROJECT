3ed6bd0ca7259ac446186dc649623f85b2d8fb40
Loulettesan <163552241+Loulettesan@users.noreply.github.com>
2024-04-02 11:19:51 +0200

    Add files via upload

--- CMI-PROJECT/ennemy.gd
+++ CMI-PROJECT/ennemy.gd
@@ -1,11 +1,14 @@
-extends CharacterBody2D
+extends PathFollow2D
 
 @export var lvl = 1
 
-@onready var speed = 300
+@onready var speed = 0.1
 @onready var atk_zone = $Area2D/AreaCollision
-@onready var collision = $BodyCollision
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
-	move_and_slide()
+	move(delta)
+
+func move(delta):
+	$AnimationPlayer.play("move")
+	$AnimationPlayer.advance(delta*speed)
