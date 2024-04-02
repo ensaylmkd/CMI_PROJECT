a67aa50e8a9dbdfffab2b12aa750c2fa4b33ab0d
Loulettesan <163552241+Loulettesan@users.noreply.github.com>
2024-04-02 15:24:20 +0200

    Add files via upload

--- CMI-PROJECT/bullet.gd
+++ CMI-PROJECT/bullet.gd
@@ -1,10 +1,14 @@
 extends RigidBody2D
 
+@export var cible = ""
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	pass
+
+# Voir pour implémenter target dans _process pour avoir acces à delta
 
 func target(t):
 	var direction = (t.global_position - self.global_position)
--- CMI-PROJECT/ennemy.gd
+++ CMI-PROJECT/ennemy.gd
@@ -1,13 +1,17 @@
 extends PathFollow2D
 
-@export var lvl = 1
+@export var type = 1
 
-@onready var speed = 0.1
+@onready var speed = randf_range(0.001, 0.03)
 @onready var atk_zone = $Area2D/AreaCollision
 @onready var anim = $AnimatedSprite2D
 
+func _ready():
+	$AnimationPlayer.play("static")
+
 func _physics_process(delta):
-	move(delta)
+	if $AnimationPlayer.is_playing():
+		move(delta)
 
 func move(delta):
 	$AnimationPlayer.play("move")
