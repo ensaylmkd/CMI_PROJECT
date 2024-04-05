129f1ae68db177560af84e91392db01d1a724232
AnagamiZz <123752557+AnagamiZz@users.noreply.github.com>
2024-04-05 15:20:23 +0200

    Add files via upload

---
+++ CMI-PROJECT/Control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://draft.tscn")
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	#get_tree().change_scene_to_file("res://options.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
